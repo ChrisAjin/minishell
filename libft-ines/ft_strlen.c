befcdfbdb4b62573eb9ee0f3cd78e87bfc4ed31b
Ines-be <bennounaines21@gmail.com>
2024-06-25 17:55:17 +0200

    git restore ok

--- libft-ines/ft_strlen.c
+++ libft-ines/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: inbennou <inbennou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 17:39:12 by inbennou          #+#    #+#             */
-/*   Updated: 2024/06/25 16:53:18 by inbennou         ###   ########.fr       */
+/*   Updated: 2024/06/25 17:51:53 by inbennou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	ft_strlen(const char *str)
 	int	i;
 
 	i = 0;
-	if (!str) // Vérification si le pointeur est NULL
-        	return 0;
 	while (str[i])
 		i++;
 	return (i);
